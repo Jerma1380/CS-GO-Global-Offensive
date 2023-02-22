73d0b1779904e358cf5f2353c7b9749b15212806
Jerma1380 <125635396+Jerma1380@users.noreply.github.com>
2023-02-22 21:58:32 +0300

    ПР2_задание1

---
+++ Triangle.cpp
@@ -0,0 +1,28 @@
+#include "Triangle.h";
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+bool Triangle::exst_tr() {
+	return ((a + b > c) && (a + c > b) && (b + c > a));
+}
+void Triangle::set(double a1, double b1, double c1) {
+	a = a1;
+	b = b1;
+	c = c1;
+}
+void Triangle::show() {
+	cout << "1 сторона равна: " << a << endl;
+	cout << "2 сторона равна: " << b << endl;
+	cout << "3 сторона равна: " << c << endl;
+}
+double Triangle::perimetr() {
+	double p = a + b + c;
+	return p;
+}
+double Triangle::square() {
+	double p = (a + b + c) / 2;
+	double s = sqrt(p * (p - a) * (p - b) * (p - c));
+	return s;
+}
